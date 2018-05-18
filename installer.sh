360d2ff772e5bd6d32e39672b0d1cbb0e8a66806
M3-SECURITY <m3-sec@protonmail.com>
2018-05-18 15:32:35 +0200

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo chmod +x searx.sh
+cd
+"" >> .bashrc
+echo "alias searx='bash ~/searx/searx.sh'" >> .bashrc
+touch .bashrc
+clear
+sudo apt-get install git build-essential libxslt-dev python-dev python-virtualenv python-babel zlib1g-dev libffi-dev libssl-dev
+cd /usr/local
+sudo git clone https://github.com/asciimoo/searx.git
+sudo useradd searx -d /usr/local/searx
+sudo chown searx:searx -R /usr/local/searx
+cd /usr/local/searx
+virtualenv searx-ve
+. ./searx-ve/bin/activate
+./manage.sh update_packages
+sed -i -e "s/ultrasecretkey/`openssl rand -hex 16`/g" searx/settings.yml
+clear
+echo "Everything done! To use the tool run ./searx.sh"
---
+++ searx.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "                                                                                           ";
+echo "                                                                                           ";
+echo "   SSSSSSSSSSSSSSS                                                                         ";
+echo " SS:::::::::::::::S                                                                        ";
+echo "S:::::SSSSSS::::::S                                                                        ";
+echo "S:::::S     SSSSSSS                                                                        ";
+echo "S:::::S                eeeeeeeeeeee    aaaaaaaaaaaaa  rrrrr   rrrrrrrrrxxxxxxx      xxxxxxx";
+echo "S:::::S              ee::::::::::::ee  a::::::::::::a r::::rrr:::::::::rx:::::x    x:::::x ";
+echo " S::::SSSS          e::::::eeeee:::::eeaaaaaaaaa:::::ar:::::::::::::::::rx:::::x  x:::::x  ";
+echo "  SS::::::SSSSS    e::::::e     e:::::e         a::::arr::::::rrrrr::::::rx:::::xx:::::x   ";
+echo "    SSS::::::::SS  e:::::::eeeee::::::e  aaaaaaa:::::a r:::::r     r:::::r x::::::::::x    ";
+echo "       SSSSSS::::S e:::::::::::::::::e aa::::::::::::a r:::::r     rrrrrrr  x::::::::x     ";
+echo "            S:::::Se::::::eeeeeeeeeee a::::aaaa::::::a r:::::r              x::::::::x     ";
+echo "            S:::::Se:::::::e         a::::a    a:::::a r:::::r             x::::::::::x    ";
+echo "SSSSSSS     S:::::Se::::::::e        a::::a    a:::::a r:::::r            x:::::xx:::::x   ";
+echo "S::::::SSSSSS:::::S e::::::::eeeeeeeea:::::aaaa::::::a r:::::r           x:::::x  x:::::x  ";
+echo "S:::::::::::::::SS   ee:::::::::::::e a::::::::::aa:::ar:::::r          x:::::x    x:::::x ";
+echo " SSSSSSSSSSSSSSS       eeeeeeeeeeeeee  aaaaaaaaaa  aaaarrrrrrr         xxxxxxx      xxxxxxx";
+echo "                                                                                           ";
+echo "                                                                                           ";
+echo "                                                                                           ";
+echo "                                                                                           ";
+echo "                                                                                           ";
+echo "                                                                                           ";
+echo "                                                                                           ";
+
+echo " Go to http://localhost:8888"
+python searx/webapp.py
